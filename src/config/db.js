b1f02488a3f7b48427de46e4668e3dee17202133
WaseemAkram <wasimakram03004@gmail.com>
2026-03-05 08:34:02 +0530

    fix env variable name

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce';
+    const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce';
     await mongoose.connect(uri);
     console.log('MongoDB connected');
   } catch (err) {
