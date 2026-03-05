2f0dde9e8e7ef9434405fcd09403182cbf3b9324
WaseemAkram <wasimakram03004@gmail.com>
2026-03-05 07:34:50 +0530

    added dynamic port for render deployment

--- index.js
+++ index.js
@@ -16,11 +16,8 @@ app.use('/api/products', productRoutes);
 app.get('/', (req, res) => {
   res.send('E-commerce catalog API is running');
 });
-
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
